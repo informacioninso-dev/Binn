bb3ef94a07b553caa1d20f093aef8b79f1fa7fe4
Alexander <alexander.fran2023@gmail.com>
2025-10-28 00:45:45 -0500

    V_0.2

---
+++ core/homepage/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core.homepage.views import homepage
+
+# creo las rutas de la app homepage para poder llamarla en urls.py de config
+# aqui llamo a la vista homepage configurada en views.py de la app homepage
+urlpatterns = [
+    path('1/', homepage),
+    path('2/', homepage),
+]
